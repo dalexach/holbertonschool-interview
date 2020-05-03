f0e8e757c33997fcd508b420259b64a124804f20
dalexach <dalexach@gmail.com>
2020-05-03 04:38:25 +0000

    Betty style correction

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->parent = parent;
 	new->left = NULL;
