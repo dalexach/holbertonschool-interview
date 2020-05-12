ca8cce2cea27af1c8dc672aaace1a402601d7976
dalexach <dalexach@gmail.com>
2020-05-12 19:40:31 +0000

    minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Function minOperations
+"""
+
+
+def minOperations(n):
+    """
+    Method that calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    Arguments:
+     - n (int): Given a number
+    Returns:
+     Returns an integer
+    """
+    if type(n) is not int or n < 2:
+        return 0
+
+    factor = []
+    i = 2
+    while i <= n:
+        if n % i == 0:
+                factor.append(i)
+                n = n / i
+        else:
+            i += 1
+
+    return sum(factor)
