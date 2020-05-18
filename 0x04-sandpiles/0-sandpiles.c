df5991cb1b97631c1fb6f7de55879a6c1ad69509
dalexach <dalexach@gmail.com>
2020-05-18 21:13:50 +0000

    First information

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Function that computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ * Return: Nothing (void)
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+}
