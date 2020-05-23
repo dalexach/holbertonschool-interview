0d57006455bbeb17362cc194c1726aa1b87664fc
dalexach <dalexach@gmail.com>
2020-05-23 20:57:55 +0000

    First information

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Function that checks if a singly linked list is a palindrome
+ * @head: head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+}
