4ee0ecdd97046f6920a68b4d502100860aeb7b22
dalexach <dalexach@gmail.com>
2020-06-04 03:45:50 +0000

    PEP8 style correction

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,7 +7,7 @@ Script that reads stdin line by line and computes metrics
 import sys
 
 
-status_codes = {'200': 0, '301': 0, '400':0, '401': 0, '403': 0, '404': 0,
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
                 '405': 0, '500': 0}
 counter = 0
 size = 0
