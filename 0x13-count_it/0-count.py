dd3c0576e05b1e9ab2cec279f36d158fc4275783
dalexach <dalexach@gmail.com>
2020-09-16 03:41:06 +0000

    PEP8 correction

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -17,7 +17,8 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     agent = {"User-Agent": "linux:1:v2.1 (by /u/heimer_r)"}
     payload = {"limit": "100", "after": after}
-    hot = requests.get(url, headers=agent, params=payload, allow_redirects=False)
+    hot = requests.get(url, headers=agent,
+                       params=payload, allow_redirects=False)
 
     if hot.status_code == 200:
         posts = hot.json().get("data").get("children")
