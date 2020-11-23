351f0a718dc94de6e4695cfd1c5e1c6dc69ee761
dalexach <dalexach@gmail.com>
2020-11-23 22:41:17 +0000

    Uploading the first information of the project

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * radix_sort - Function that sorts an array of integers in ascending order
+ * using the Radix sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: Always 0
+ */
+
+ void radix_sort(int *array, size_t size)
