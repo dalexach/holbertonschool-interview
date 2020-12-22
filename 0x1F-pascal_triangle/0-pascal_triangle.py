8d02f86d41743e03f4212ebfaddf32995b51920c
dalexach <dalexach@gmail.com>
2020-12-22 14:45:07 +0000

    Uploading first information

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of lists of integers
+    representing the Pascal’s triangle of n
+
+    Arguments:
+     - n will be always an integer
+
+    Returns:
+     An empty list if n <= 0
+    """
