51c9adffec044afa6777ca8e4edd74cc528d6a96
dalexach <dalexach@gmail.com>
2020-12-22 14:48:57 +0000

    Uploading the solution

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -15,3 +15,16 @@ def pascal_triangle(n):
     Returns:
      An empty list if n <= 0
     """
+
+    pascal = []
+    for i in range(1, n + 1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                row.append(1)
+            else:
+                n = pascal[i - 2][j - 1] + pascal[i - 2][j]
+                row.append(n)
+        pascal.append(row)
+
+    return pascal
